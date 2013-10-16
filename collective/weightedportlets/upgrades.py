0738eb639c39e5fe8432ec68bc3960f78b2af80c
Adam Castle <acastle@ripe.net>
2013-10-16 08:17:11 +0200

    removed commit call

--- collective/weightedportlets/upgrades.py
+++ collective/weightedportlets/upgrades.py
@@ -1,5 +1,4 @@
 from Products.CMFCore.utils import getToolByName
-from transaction import commit
 
 default_profile = 'profile-collective.weightedportlets:default'
 
@@ -8,4 +7,3 @@ def upgrade_from_kss(portal):
     print "Upgrade collective.weightedportlets to use Ajax"
     portal_setup = getToolByName(portal, 'portal_setup')
     portal_setup.runImportStepFromProfile(default_profile, 'jsregistry')
-    commit()
